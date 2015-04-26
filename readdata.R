d98b7de4fae43cb30f6cdf635c8d527100117c57
Andrew Dyck <info@andrewdyck.com>
2015-04-26 13:40:38 -0600

    reading data

---
+++ readdata.R
@@ -0,0 +1,9 @@
+
+
+# read data
+
+setwd('~/Downloads/exdata-data-NEI_data/')
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+setwd('~/Documents/exdata_2/')
